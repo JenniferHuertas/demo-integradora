16c1f2c629e07eece7ac4d7d3c518ac5d40d68a0
Jennifer Huertas <jennifersu2059@gmail.com>
2025-12-11 21:12:50 -0500

    cierre middleware

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { LoggerMiddleware } from 'src/middlewares/loger.middleware';
 
 @Module({
   providers: [UsersService],
+  controllers: [UsersController],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('users');
+  }
+}
